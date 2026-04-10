249ca2ba711bc6e9544809b801f00adc16101e96
MOkeke-create <Michaelokeke123@gmail.com>
2026-04-10 10:37:56 -0500

    feat: added multiplication result

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -10,12 +10,20 @@ public class MathApp {
 
         Scanner input = new Scanner(System.in);
         System.out.print("Enter number 1: ");
-        float num1 = input.nextFloat();
+        double num1 = input.nextDouble();
         System.out.print("Enter number 2: ");
-        float num2 = input.nextFloat();
+        double num2 = input.nextDouble();
         input.nextLine();
+        double calculatorResult = num1 * num2;
+
+
         System.out.println("Possible calculations: ");
-        System.out.println();
+        System.out.println("Enter:\n (A)dd\n (S)ubtract\n (M)ultiply\n (D)ivide ");
+        System.out.println("Processing....... ");
+        System.out.println("Multiplication chosen");
+        System.out.println("The result is " + calculatorResult);
+
+
 
 
 
