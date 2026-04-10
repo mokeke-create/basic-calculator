37c07838d9d07e980906f77efbefe05b8b6ebe8c
MOkeke-create <Michaelokeke123@gmail.com>
2026-04-10 10:18:24 -0500

    feat: initiated scanner

---
+++ src/main/java/com/pluralsight/MathApp.java
@@ -0,0 +1,27 @@
+package com.pluralsight;
+
+import org.w3c.dom.ls.LSInput;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class MathApp {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number 1: ");
+        float num1 = input.nextFloat();
+        System.out.print("Enter number 2: ");
+        float num2 = input.nextFloat();
+        input.nextLine();
+        System.out.println("Possible calculations: ");
+        System.out.println();
+
+
+
+
+
+
+
+    }
+}
